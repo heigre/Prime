22bf59ca7ed53fd5780f6aa82dadab5022e8a9c4
Mats Heigre@Venera <mfheigre@gmail.com>
2017-01-19 00:51:35 +0100

    Changelog: Added correct working files

---
+++ primechecker.py
@@ -0,0 +1,34 @@
+n = []
+checker = []
+x = 213
+for y in range (2, x+1): 
+    z = x/(float(y))
+    checker.append(z)
+    #print checker
+    print z
+
+
+for k in range (0, x-1):
+    p = checker[k]
+    if p.is_integer() == True:
+        n.append(1)
+    else:
+        s=0
+
+    if sum(n) > 1:
+        print ""
+        print ""
+        print ""
+        print n
+        print x
+        print "Sadly, it is not a prime"
+        break
+
+if sum(n) == 1:
+    print ""
+    print ""
+    print ""
+    print n
+    print x
+    print "It is a prime!"
+
---
+++ primefinder3.py
@@ -0,0 +1,78 @@
+primes = []
+
+def primecheck(x):
+    n = []
+    checker = []
+    for y in range (2, x+1): 
+        z = x/(float(y))
+        checker.append(z)
+        #print checker
+        #print z
+
+
+    for k in range (0, x-1):
+        p = checker[k]
+        if p.is_integer() == True:
+            n.append(1)
+        else:
+            s=0
+
+        if sum(n) > 1:
+            return 0
+
+    if sum(n) == 1:
+        return x
+
+u = input("Type the highest number you want to check for primes: ")
+l = input("Type the lowest number you want to check for primes: ")
+u = int(u)
+l = int(l)
+for x in range (l, u):
+    primes.append(primecheck(x))
+    primes = [m for m in primes if m != 0]
+
+    #print (primes)
+
+print ("")
+print ("")
+print ("")
+print ("")
+print ("")
+print ("")
+print ("")
+print ("")
+print ("")
+print ("")
+print ("")
+print ("")
+print ("")
+print ("")
+print ("")
+print ("")
+print ("")
+print ("")
+print ("")
+print ("")
+print ("")
+print ("")
+print ("")
+print ("")
+print ("")
+print ("")
+print ("")
+print ("")
+print ("")
+print ("")
+print ("")
+print ("")
+print ("")
+print ("")
+print ("")
+print ("")
+print ("")
+print ("")
+print ("Prime number(s) between the entered numbers: ")
+print (primes)
+print ("")
+print ("Total number of primes:")
+print (len(primes))
